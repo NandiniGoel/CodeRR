db1a990e87385d8a45f4697a62fe8d465f2614fa
Nandini Goel <nandinigoel.india@gmail.com>
2013-12-13 07:15:47 -0800

    Presence Validated

--- app/models/button.rb
+++ app/models/button.rb
@@ -1,3 +1,4 @@
 class Button < ActiveRecord::Base
   attr_accessible :background, :border, :borderadius, :bordercolor, :color, :link, :padding, :text
+  validates_the_presence_of :background, :border, :color, :text
 end
