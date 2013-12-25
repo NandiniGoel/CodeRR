49fcfb195bcea59fb31331f630e02243a424b581
Nandini Goel <nandinigoel.india@gmail.com>
2013-12-25 12:43:56 +0530

    DFDF

--- app/models/button.rb
+++ app/models/button.rb
@@ -2,5 +2,5 @@ class Button < ActiveRecord::Base
   attr_accessible :background, :border, :borderadius, :bordercolor, :color, :link, :padding, :text, :user_id
   validates_presence_of :background, :border, :color, :text
   belongs_to :user
-end
+
 end
